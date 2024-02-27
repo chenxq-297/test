2eb52f3e1495197bbfb96981661c604ab52468a9
chenxq-297 <1037216783@qq.com>
2024-02-27 11:42:24 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/test/",
   plugins: [
     vue(),
   ],
-  base: "/test/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
